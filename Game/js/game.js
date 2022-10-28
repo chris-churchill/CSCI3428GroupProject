7da582fdfcb72d3bdddc40587cf2c0357ec3909e
Romina03 <86558548+Romina03@users.noreply.github.com>
2022-10-27 23:54:01 -0300

    Update game.js

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,34 +1,34 @@
 /*
-  The purpose of this file is to generate 9 screens one after another,
+  The purpose of this file is to generate 4 screens one after another,
   each containing 3 images, where one image must match the associated audio
   that can be played.
-  When all 9 screens have been played, the software goes back to the first
+  When all 4 screens have been played, the software goes back to the first
   screen, and starts the process over again.
   Authors: Terry Goldsmith (Created fully functioning code
-
          : Chris Churchill (revised code to apply to woodland conservation site)
+         : Romina Rahman (revised code)
 */
 
+
+// global array which has the questions
 let questions = [
  "Which one is the Yellow Birch Tree?",
  "Which one is the Apple Tree?",
  "What one is the Pear Tree?",
  "What should you check to find directions?"
 ];
- 
- 
- 
- // global array containing the questions for the game
+
+ // global array containing the answers for the game
  // Note: Each screen contains 3 images: image 1, image 2 and image 3 from
  //       left to right or from top to bottom depending on your device.
  let answers = [1, 2, 3, 2];
- 
- // global array containing 9 sets of 3 images making up the 9 screens.
+
+ // global array containing 4 sets of 3 images making up the 9 screens.
  // There are 27 elements in this array.
 let panels = [
   "Yellow Birch.jpg",
-  "Random1.jpg",             
-  "Random2.jpg",             
+  "Random1.jpg",
+  "Random2.jpg",
   "Pear Tree.jpg",
   "Apple Tree.jpg",
   "Random2.jpg",
@@ -40,10 +40,10 @@ let panels = [
   "Well.jpg",
   "Apple Tree.jpg"
 ];
- 
+
  // Contains the current screen number starting at zero.
  let panelSet = 0;
- 
+
  /*
    The purpose of this function is to inject HTML that consists of:
      - 3 images
@@ -56,32 +56,24 @@ let panels = [
  function setup() {
 
   document.getElementById("correctgif").style.visibility = "hidden";
-  
+
   document.getElementById("heading").innerHTML = questions[panelSet];
- 
+
    let str1 = '<input class="button" type="image" src="./pics/';
    let str2 = '" width="325" height="325" onclick="choose(1)"/>';
    let str3 = str1 + panels[0] + str2;
    document.getElementById("pic1").innerHTML = str3;
- 
+
    str2 = '" width="325" height="325" onclick="choose(2)"/>';
    str3 = str1 + panels[1] + str2;
    document.getElementById("pic2").innerHTML = str3;
- 
+
    str2 = '" width="325" height="325" onclick="choose(3)"/>';
    str3 = str1 + panels[2] + str2;
    document.getElementById("pic3").innerHTML = str3;
  }
- 
- 
- /*
-   The purpose of this function is to quit the application.
-   Authors: Terry Goldsmith
- */
- function quit() {
-   window.close();
- }
- 
+
+
  /*
    The purpose of this function is to produce a "try again" alert if the
    wrong image was clicked on, and to produce a "congratulations" alert if
@@ -99,21 +91,22 @@ let panels = [
  function choose(choice) {
    if (choice == answers[panelSet]) {
     document.getElementById("correctgif").style.visibility = "visible";
- 
+    alert("Congratulations!");
+
      // update panelSet in the sequence: 0 1 2 3 4 5 6 7 8 and back to 0
      panelSet = (panelSet + parseInt(1)) % parseInt(4);
- 
+
      document.getElementById("heading").innerHTML = questions[panelSet];
- 
+
      let str1 = '<input class="button" type="image" src="./pics/';
      let str2 = '" width="325" height="325" onclick="choose(1)"/>';
      let str3 = str1 + panels[panelSet * 3] + str2;
      document.getElementById("pic1").innerHTML = str3;
- 
+
      str2 = '" width="325" height="325" onclick="choose(2)"/>';
      str3 = str1 + panels[panelSet * 3 + parseInt(1)] + str2;
      document.getElementById("pic2").innerHTML = str3;
- 
+
      str2 = '" width="325" height="325" onclick="choose(3)"/>';
      str3 = str1 + panels[panelSet * 3 + parseInt(2)] + str2;
      document.getElementById("pic3").innerHTML = str3;
